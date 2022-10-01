2dd3d5dddc3517e16d0d6a922520fcfd16edc5ad
Joeri Abbo <joeriabbo@hotmail.com>
2022-10-01 22:18:18 +0200

    Add poc for speech to text.

---
+++ src/SpeechInput.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import useSpeechToText from 'react-hook-speech-to-text';
+
+function SpeechInput() {
+    const {
+        error,
+        interimResult,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+    } = useSpeechToText({
+        continuous: true,
+        useLegacyResults: false
+    });
+
+    if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+    return (
+        <div>
+            <h1>Recording: {isRecording.toString()}</h1>
+            <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+                {isRecording ? 'Stop Recording' : 'Start Recording'}
+            </button>
+            <ul>
+                {results.map((result: any) => (
+                    <li key={result.timestamp}>{result.transcript}</li>
+                ))}
+                {interimResult && <li>{interimResult}</li>}
+            </ul>
+        </div>
+    );
+}
+
+export default SpeechInput;
